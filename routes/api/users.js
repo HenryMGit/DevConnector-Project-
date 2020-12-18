641d70f3142cb2f5b561c7037159e3542175be14
HenryMGit <hm263@njit.edu>
2020-12-18 11:56:45 -0500

    Added validation to post request to /api/users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
+//User Model
+const User = require("../../models/User");
 
-//@route  GET api/users
-//@desc   Test route 
+//@route  POST api/users
+//@desc   Register User
 //@access Public
-router.get('/', (req,res) =>res.send('User Route'));
+router.post(
+  "/",
+  [
+    body("name", "Name is required").not().isEmpty(),
+    body("email", "Please include a valid email").isEmail(),
+    body(
+      "password",
+      "Please enter a password with 6 or more caharacters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+    //TO DO:
+    //See user exist
+    //Get users gravatar
+    //Encrypt password
+    //Return jsonwebtoken
+
+    res.send("User Route");
+  }
+);
 
 module.exports = router;
